7a30cc0fade99cf55fd6944ac4c6240e153f210f
Scott Harper <AberrantWolf@users.noreply.github.com>
2023-05-10 00:13:22 +0900

    Initial culmination of Microsoft's DX12 'hello X' tutorials

---
+++ PlatformIncludes.h
@@ -0,0 +1,23 @@
+//
+// Created by lareo on 5/1/2023.
+//
+
+#ifndef SCRIBBLEKIT_PLATFORMINCLUDES_H
+#define SCRIBBLEKIT_PLATFORMINCLUDES_H
+
+
+#ifdef WIN32
+#include <windows.h>
+
+#define MAIN_ARGS HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow
+#define APP_ARGS_DECL HINSTANCE hInstance, int nCmdShow
+#define APP_ARGS hInstance, nCmdShow
+#define MainFunc() int WINAPI WinMain(MAIN_ARGS)
+
+#else
+
+#define MainFunc() int main()
+
+#endif
+
+#endif //SCRIBBLEKIT_PLATFORMINCLUDES_H
---
+++ main.cpp
@@ -0,0 +1,76 @@
+//
+// Created by Scott Harper on 5/1/2023.
+//
+
+//#include "IApplication.h"
+//
+//MainFunc() {
+//    scribble::InitApplication(APP_ARGS);
+//    return scribble::GetApplication()->Run();
+//}
+
+#include <GLFW/glfw3.h>
+#include <cstdio>
+#include <iostream>
+
+#include "GraphicsApiInterface.h"
+
+void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+int main()
+{
+    if (!glfwInit())
+    {
+        // Initialization failed
+        return -1;
+    }
+    printf("Initialized\n");
+
+    glfwSetErrorCallback(error_callback);
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(720, 480, "Hi There", NULL, NULL);
+    if (!window)
+    {
+        return -2;
+    }
+    printf("Window created\n");
+
+    glfwSetKeyCallback(window, key_callback);
+
+    scribble::GraphicsApiInterface* pInterface = scribble::MakeGraphicsApi();
+    printf("Api interface created\n");
+
+    try {
+        pInterface->Init(window, "shaders");
+    } catch (const std::exception& e) {
+        std::cout << "Error initializing graphics api: " << e.what() << std::endl;
+        return -3;
+    }
+    printf("Api interface initialized\n");
+
+    while (!glfwWindowShouldClose(window))
+    {
+        // Keep running
+        glfwPollEvents();
+
+        pInterface->Update();
+        pInterface->Render();
+    }
+
+    // Shutdown
+    pInterface->Destroy();
+    delete pInterface;
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
